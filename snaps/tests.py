62fc5ab1cb519a1a08940514042a15b0258d3bb6
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-03 12:57:57 +0300

    created first image test class

--- snaps/tests.py
+++ snaps/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Image,Location,Category
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(location = 'dubai')
+        self.location.save()
+        self.category = Category(category = 'travel')
+        self.category.save()
+        self.new_image=Image(image_link = 'snaps/img.png',name = 'car',description = 'An exclusive picture of the limited edition range rover velar',location= self.location,category=self.category)
+        self.new_image.save()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
