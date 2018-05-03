ae9121ac97f26e052398e36ce51325187fe66555
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-03 12:22:12 +0300

    migrated new models

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'faith',
+        'PASSWORD':'password',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- snaps/models.py
+++ snaps/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_link = models.ImageField(upload_to ='snaps/')
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    location = models.ForeignKey('Location',null = True)
+    category = models.ForeignKey('Category', null =True)
+class Location(models.Model):
+    location = models.CharField(max_length = 60)
+class Category(models.Model):
+    category = models.CharField(max_length = 60)
