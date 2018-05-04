cf8ef40d94b0b1c5f3f242472a4749aa7165efe6
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-04 08:58:55 +0300

    added bootstrap3

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snaps.apps.SnapsConfig',
+    'snaps',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
