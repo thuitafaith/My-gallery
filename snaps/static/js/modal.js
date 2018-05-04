8530a99e0481fd7ce191a4b16e13e109cd5c4e36
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-04 18:05:58 +0300

    added modal functionality

--- snaps/static/js/modal.js
+++ snaps/static/js/modal.js
@@ -1,20 +1,13 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+$(document).ready(function(){
+$('.my_Img').click(function(){
+    $('#Modal_pic').css('display',"block")
+    $("#img1").attr('src',$(this).attr('src'));
+    $("#caption").html() = $(this).attr('alt');
+})
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$(".close").click(function() {
+  $('#Modal_pic').css('display',"none");
+  })
+
+})
